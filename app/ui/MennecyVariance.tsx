201d2a48293d8b047d0e9245979edbb9076b5a98
AlexSearha <contact@mytechcompanion.fr>
2024-05-20 17:07:37 +0200

    Add text responsive

--- app/ui/MennecyVariance.tsx
+++ app/ui/MennecyVariance.tsx
@@ -9,13 +9,13 @@ export default function MennecyVariance() {
   return (
     <div
       id="variance-promo"
-      className="relative text-white w-full px-6 py-36 h-auto gap-6 flex flex-col items-center"
+      className="relative text-white w-full px-6 py-12 h-auto gap-6 flex flex-col items-center md:py-36"
     >
       <div className="absolute w-full h-full top-0 left-0 bg-varianceCover bg-cover bg-center brightness-50 blur-sm"></div>
       <div className="relative w-full">
         <div>
-          <div className="relative flex w-full items-center justify-around">
-            <div className="relative w-[450px] h-[450px] shadow-lg shadow-black flex">
+          <div className="relative flex flex-col-reverse gap-10 w-full items-center justify-around xl:flex-row">
+            <div className="w-auto h-[350px] -translate-x-[23%] shadow-lg shadow-black flex xl:translate-x-0 xl:h-[450px]">
               <Image
                 src={VarianceCover}
                 layout="fill"
@@ -23,7 +23,7 @@ export default function MennecyVariance() {
                 alt="variance cover"
                 className="z-20"
               />
-              <div className="absolute top-0 left-[200px] w-[450px] h-[450px] ">
+              <div className="w-[350px] h-[350px] translate-x-[50%] z-10 xl:w-[450px] xl:h-[450px]">
                 <Image
                   src={VinylImage}
                   layout="fill"
@@ -31,10 +31,10 @@ export default function MennecyVariance() {
                   alt="variance cover"
                   className="animate-spin-slow z-10"
                 />
-                <div className="absolute z-0 top-[125px] left-[125px] w-[250px] h-[250px] bg-black"></div>
+                <div className="absolute z-0 top-[100px] left-[100px] w-[150px] h-[150px] bg-black xl:top-[125px] xl:left-[150px] xl:w-[200px] xl:h-[200px]"></div>
               </div>
             </div>
-            <div id="" className="text-center">
+            <div className="text-center">
               <h2 className="font-bold text-5xl">
                 GET PHYSICAL <br />
                 <span className="text-[6rem] bg-gradient-to-r from-startVariancesColor to-endVariancesColor inline-block text-transparent bg-clip-text">
@@ -44,7 +44,7 @@ export default function MennecyVariance() {
                 <span className="text-4xl">ONLY AT</span>
                 <br />
                 <Link
-                  className="text-7xl animate-pulse bg-gradient-to-r from-startMennecyColor to-endMennecyColor inline-block text-transparent bg-clip-text transition-all hover:animate-none"
+                  className="text-5xl xl:text-7xl animate-pulse bg-gradient-to-r from-startMennecyColor to-endMennecyColor inline-block text-transparent bg-clip-text transition-all hover:animate-none"
                   href="https://www.fnactickets.com/ticket-evenement/hard-rock-metal-mennecy-metal-fest-man64884-lt.htm"
                   target="_blank"
                 >
