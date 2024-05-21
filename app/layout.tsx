8a957c1773cc8e10d2507d66c7e4c5cbf390fea7
AlexSearha <contact@mytechcompanion.fr>
2024-05-21 22:36:50 +0200

    change photos band

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { oswald, poppins } from "./lib/font"
+import { oswald } from "./lib/font"
 import "./globals.css"
 
 export const metadata: Metadata = {
