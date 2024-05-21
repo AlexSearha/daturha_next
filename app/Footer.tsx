5946b609c7e551c64d877a031edfcbff17283d0e
AlexSearha <contact@mytechcompanion.fr>
2024-05-22 00:12:51 +0200

    fix divers

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,17 +3,15 @@ import SocialsBar from "./ui/SocialsBar"
 export default function Footer() {
   const actualYear = new Date().getFullYear()
   return (
-    <>
-      <div className="bg-text h-44">
-        <div className="flex flex-col gap-8 justify-center items-center h-full">
-          <div id="socials" className="flex gap-6">
-            <SocialsBar />
-          </div>
-          <p className="text-center text-sm text-white">
-            {`© ${actualYear} Daturha. All rights reserved.`}
-          </p>
+    <div className="bg-text h-44">
+      <div className="flex flex-col gap-8 justify-center items-center h-full">
+        <div id="socials" className="flex gap-6">
+          <SocialsBar />
         </div>
+        <p className="text-center text-sm text-white">
+          {`© ${actualYear} Daturha. All rights reserved.`}
+        </p>
       </div>
-    </>
+    </div>
   )
 }
