4c5677e7466c9a7d8726cf61e6ed5525319aa2bd
AlexSearha <contact@mytechcompanion.fr>
2024-06-12 00:09:06 +0200

    change adress extension mailto

--- app/ui/ContacSection.tsx
+++ app/ui/ContacSection.tsx
@@ -14,19 +14,19 @@ export default function ContactSection() {
         <div id="managment-container" className="flex flex-col items-center">
           <h3 className="text-2xl text-gray-600">Managment</h3>
           <a
-            href="mailto:managment@daturha.fr"
+            href="mailto:managment@daturha.com"
             className="transition text-xl hover:text-gray-600"
           >
-            managment@daturha.fr
+            managment@daturha.com
           </a>
         </div>
         <div id="booking-container" className="flex flex-col items-center">
           <h3 className="text-2xl text-gray-600">Booking</h3>
           <a
-            href="mailto:booking@daturha.fr"
+            href="mailto:booking@daturha.com"
             className="transition text-xl hover:text-gray-600"
           >
-            booking@daturha.fr
+            booking@daturha.com
           </a>
         </div>
       </div>
